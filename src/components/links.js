d7a9fc5ec9dbf686081206a3e7de6a2e4a8c7daa
Tonny Oseko <tonnyoseko0@gmail.com>
2022-10-30 01:35:32 +0300

    style the links section

--- src/components/links.js
+++ src/components/links.js
@@ -1,11 +1,94 @@
 import React from "react";
 
 function Links() {
-    return(
-        <button type="button" class="btn btn-secondary btn-lg btn-block" id="twitter">
-            <a href="https://twitter.com/TonyOseko" target="_blank" rel="noopener noreferrer">Twitter Link</a>
-        </button>
-    )
+  return (
+    <div className="container" id="links-container">
+      {/* twitter link */}
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        id="twitter"
+      >
+        <a
+          href="https://twitter.com/TonyOseko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter Link
+        </a>
+      </button>
+      {/* zuri team */}
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        id="twitter"
+      >
+        <a
+          href="https://twitter.com/TonyOseko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Zuri Team
+        </a>
+      </button>
+      {/* zuri books */}
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        id="twitter"
+      >
+        <a
+          href="https://twitter.com/TonyOseko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Zuri Books
+        </a>
+      </button>
+      {/* python books */}
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        id="twitter"
+      >
+        <a
+          href="https://twitter.com/TonyOseko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Python Books
+        </a>
+      </button>
+      {/* Background check for coders */}
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        id="twitter"
+      >
+        <a
+          href="https://twitter.com/TonyOseko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Background Check for Coders
+        </a>
+      </button>
+      {/* Design Books */}
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        id="twitter"
+      >
+        <a
+          href="https://twitter.com/TonyOseko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Design Books
+        </a>
+      </button>
+    </div>
+  );
 }
 
 export default Links;
